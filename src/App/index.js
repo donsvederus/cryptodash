b2bc0f53c86c55defe37b8a3a664e5e0512dd770
Don Svederus <svederus@gmail.com>
2022-06-12 14:18:18 -0600

    0.07.2 removed practice styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-import styled, {css} from 'styled-components';
-
-const MyButton = styled.div`
-  color: green;
-
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`
-
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+// import styled, {css} from 'styled-components';
 
 class App extends Component {
   render() {
     return (
       <div>
       <WelcomeMessage/>
-      <MyButton>Hello</MyButton>
-      <MyButton primary>Hello</MyButton>
-      <TomatoButton>Hello</TomatoButton>
       </div>
     );
   }
